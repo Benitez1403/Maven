f5cc2a4c43bfab6530240b79133a20aefef76b44
Your Name <you@example.com>
2024-05-04 23:01:29 -0600

    My second Commit

---
+++ src/main/java/com/iis/bo16004/demo_bo16004/App.java
@@ -0,0 +1,18 @@
+package com.iis.bo16004.demo_bo16004;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello World  I´m BO16004";
+	}
+	
+    public static void main( String[] args )
+    {
+    	App hw=new App();
+        System.out.println(hw.Hello());
+    }
+}
---
+++ src/test/java/com/iis/bo16004/demo_bo16004/AppTest.java
@@ -0,0 +1,17 @@
+package com.iis.bo16004.demo_bo16004;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testHello() {
+        assertTrue( true );
+    }
+}
